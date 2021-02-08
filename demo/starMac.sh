e4c300f138263f38a56dfceee0c73e7cd20edfa8
niwakoki <3218402803@qq.com>
2021-02-08 15:08:37 +0800

    update

---
+++ demo/starMac.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+ulimit -n 4096
+
+star="./macOS/STAR"
+# star="~/linuxOS/STAR"
+
+myGenomeDir="./data/genome/"
+myGenomeFastaFiles="./data/genome/chr21.hg37.fa"
+mySjdbGTFfile="./data/genome/chr21.hg37.gtf"
+myFastqFile1="./data/RNAseq/demo.chr21.1.fastq.gz"
+myFastqFile2="./data/RNAseq/demo.chr21.2.fastq.gz"
+myFileNamePrefix="./data/result/result."
+
+${star} \
+ --runThreadN 12 \
+ --genomeSAindexNbases 11 \
+ --genomeDir ${myGenomeDir} \
+ --genomeFastaFiles ${myGenomeFastaFiles} \
+ --sjdbGTFfile ${mySjdbGTFfile} \
+ --sjdbOverhang 100 \
+ --readFilesIn ${myFastqFile1} ${myFastqFile2} \
+ --readFilesCommand gunzip -c \
+ --outFileNamePrefix ${myFileNamePrefix} \
+ --outSAMtype BAM SortedByCoordinate
